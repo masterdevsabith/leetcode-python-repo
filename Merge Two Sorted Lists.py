68ce701ab82fe62a80be974b84b25fbf50c46f33
Sabith <masterdevsabith@gmail.com>
2025-07-20 12:01:26 +0530

    done some

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -15,14 +15,23 @@
 
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+        final_list = []
+
+        for i in range(len(list1)):
+            if i == 0:
+                final_list.append(list1[i])
+        for x in range(len(list2)):
+            if x == 0:
+                final_list.append(list2[x])
+        return final_list
 
 
 list = Solution()
 list1 = list.mergeTwoLists([1, 2, 4], [1, 3, 4])
 list2 = list.mergeTwoLists([], [])
 list3 = list.mergeTwoLists([], [0])
+
+
+print(f'result of first array : {list1}')
+print(f'result of second array : {list2}')
+print(f'result of third array : {list3}')
